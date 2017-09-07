9dcdfb51240411c7623f3851073dbbae9ed0ded6
ramu.n <ramu.n@huawei.com>
2017-09-07 23:33:17 +0530

    Fix sql-resource-provider compilation failure
    Fixed unresolved method DBResourceManager.create() in SqlResource.java
    
    Change-Id: Idb1c151e9929e5e9be5848b52c464e09c5ba835f
    Issue-Id: CCSDK-82
    Signed-off-by: Ramu N <ramu.n@huawei.com>

--- sql-resource/provider/src/main/java/org/onap/ccsdk/sli/adaptors/resource/sql/SqlResource.java
+++ sql-resource/provider/src/main/java/org/onap/ccsdk/sli/adaptors/resource/sql/SqlResource.java
@@ -8,9 +8,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -350,7 +350,7 @@ public class SqlResource implements SvcLogicResource, SvcLogicJavaPlugin {
 			// as a
 			// a POJO then.
 			try {
-				dblibSvc = DBResourceManager.create(System.getProperties());
+				dblibSvc = new DBResourceManager(System.getProperties());
 			} catch (Exception e) {
 				LOG.error("Caught exception trying to create dblib service", e);
 			}
