f3a007533b141852f1c6b5acc5d8bfb2b07ed502
prakash.e <prakash.e@huawei.com>
2018-03-28 16:31:50 +0530

    coverage SqlResourcePropertiesProviderImpl
    Coverage improvement for SqlResourcePropertiesProviderImpl
    https://jira.onap.org/browse/CCSDK-213?jql=project%20%3D%20CCSDK%20AND%20text%20~%20coverage
    
    Issue-ID: CCSDK-213
    Change-Id: I5e9fe5a223918d4111c937463078d5b69609f6c4
    Signed-off-by: prakash.e <prakash.e@huawei.com>

--- sql-resource/provider/src/test/java/org/onap/ccsdk/sli/adaptors/resource/sql/SqlResourceProviderTest.java
+++ sql-resource/provider/src/test/java/org/onap/ccsdk/sli/adaptors/resource/sql/SqlResourceProviderTest.java
@@ -1,7 +1,8 @@
 package org.onap.ccsdk.sli.adaptors.resource.sql;
 
 import static org.junit.Assert.assertNotNull;
-
+import java.lang.reflect.Field;
+import java.util.Map;
 import java.util.Properties;
 
 import org.junit.Test;
@@ -9,9 +10,21 @@ import org.junit.Test;
 public class SqlResourceProviderTest {
 
     private static SqlResourcePropertiesProvider provider;
+    private static final String SDNC_CONFIG_DIR = "SDNC_CONFIG_DIR";
 
     @Test
     public void testSqlResourceProvider() {
+        try{
+            Map<String, String> env = System.getenv();
+            Class<?> cl = env.getClass();
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+            writableEnv.put(SDNC_CONFIG_DIR, "./src/test/resources");
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to set environment variable", e);
+        }
+
         provider = new SqlResourcePropertiesProviderImpl();
         assertNotNull(provider);
     }
