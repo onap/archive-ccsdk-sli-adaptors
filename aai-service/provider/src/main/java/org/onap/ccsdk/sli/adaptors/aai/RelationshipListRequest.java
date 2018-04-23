5b132fb6f1e33808b3c65b23403a2211726e5398
Rich Tabedzki <richard.tabedzki@att.com>
2018-04-23 15:50:06 -0400

    Fixing trustStore issues in AAI Client
    Changes made:
    * Updated AAI XSD model to v13
    * Updated AAI interface path definitions
    
    Change-Id: I047845e574734b45dda67e1f84b3bb07dacca426
    Issue-ID: CCSDK-247
    Signed-off-by: Rich Tabedzki <richard.tabedzki@att.com>

--- aai-service/provider/src/main/java/org/onap/ccsdk/sli/adaptors/aai/RelationshipListRequest.java
+++ aai-service/provider/src/main/java/org/onap/ccsdk/sli/adaptors/aai/RelationshipListRequest.java
@@ -3,14 +3,14 @@
  * openECOMP : SDN-C
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights
- * 						reserved.
+ *                         reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,6 @@ package org.onap.ccsdk.sli.adaptors.aai;
 
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
@@ -31,38 +30,33 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.message.BasicNameValuePair;
-import org.onap.ccsdk.sli.adaptors.aai.data.AAIDatum;
-import org.openecomp.aai.inventory.v11.RelationshipList;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Joiner;
+import org.openecomp.aai.inventory.v13.RelationshipList;
 
 public class RelationshipListRequest extends GenericRequest {
-	
-	public static final String SELFLINK = "selflink";
 
-	public RelationshipListRequest(AAIRequest masterRequest) {
-		super(RelationshipList.class);
-		this.addMasterRequest(masterRequest);
-	}
+    public static final String SELFLINK = "selflink";
 
-	
-	@Override
-	public URL getRequestUrl(String method, String resourceVersion) throws UnsupportedEncodingException, MalformedURLException, URISyntaxException {
+    public RelationshipListRequest(AAIRequest masterRequest) {
+        super(RelationshipList.class);
+        this.addMasterRequest(masterRequest);
+    }
 
-		URL url = super.getRequestUrl(method, null);
-		URIBuilder builder = new URIBuilder(url.toURI());
-	    String newPath = builder.getPath() + "/relationship-list";
-	    builder.setPath(newPath);
-	    if(resourceVersion != null) {
-	    	List<NameValuePair> queryList = builder.getQueryParams();
-	    	NameValuePair nvp = new BasicNameValuePair("resourceVersion", resourceVersion);
-	    	queryList.add(nvp);
-	    }
 
-		aaiService.LOGwriteFirstTrace(method, builder.toString());
-		
-		return builder.build().toURL();
-	}
+    @Override
+    public URL getRequestUrl(String method, String resourceVersion) throws UnsupportedEncodingException, MalformedURLException, URISyntaxException {
+
+        URL url = super.getRequestUrl(method, null);
+        URIBuilder builder = new URIBuilder(url.toURI());
+        String newPath = builder.getPath() + "/relationship-list";
+        builder.setPath(newPath);
+        if(resourceVersion != null) {
+            List<NameValuePair> queryList = builder.getQueryParams();
+            NameValuePair nvp = new BasicNameValuePair("resourceVersion", resourceVersion);
+            queryList.add(nvp);
+        }
+
+        aaiService.LOGwriteFirstTrace(method, builder.toString());
+
+        return builder.build().toURL();
+    }
 }
