8fe9122e5613fcfbccfbbe1af067c6af6fe89d59
Ganesh Chandrasekaran <ganesh.c@samsung.com>
2018-06-28 17:39:09 +0900

    SaltStack adaptor API creation
    Issue-ID: CCSDK-319
    Change-Id: Iaa96550a3b246a53cd8db6431fdc649dc8032feb
    Signed-off-by: Ganesh Chandrasekaran <ganesh.c@samsung.com>

---
+++ saltstack-adapter/saltstack-adapter-provider/src/main/java/org/onap/ccsdk/sli/adaptors/saltstack/SaltstackAdapter.java
@@ -0,0 +1,65 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.adaptors.saltstack;
+
+import java.util.Map;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicJavaPlugin;
+
+/**
+ * This interface defines the operations that the Saltstack adapter exposes.
+ *
+ */
+public interface SaltstackAdapter extends SvcLogicJavaPlugin {
+    /**
+     * Returns the symbolic name of the adapter
+     *
+     * @return The adapter name
+     */
+    String getAdapterName();
+
+    /* Method to post a single command request for execution on SaltState server
+     *  The response from Saltstack comes in json format and it is automatically put
+     *  to context for DGs access, with a certain prefix*/
+    void reqExecCommand(Map<String, String> params, SvcLogicContext ctx) throws SvcLogicException;
+
+    /* When SLS file is created/available then this Method can be used to post
+     *  the file to saltstack server and execute the SLS file on SaltState server
+     *  The response from Saltstack comes in json format and it is automatically put
+     *  to context for DGs access, with a certain prefix*/
+    void reqExecSLS(Map<String, String> params, SvcLogicContext ctx) throws SvcLogicException;
+
+    /* Method to get log of a saltState execution request
+     *  The response from Saltstack comes in json format and it is automatically put
+     *  to context for DGs access, with a certain prefix*/
+    void reqExecLog(Map<String, String> params, SvcLogicContext ctx) throws SvcLogicException;
+
+    /**
+     * Set the command execution timeout
+     * @param timeout time in milliseconds
+     */
+    void setExecTimeout(long timeout);
+}
