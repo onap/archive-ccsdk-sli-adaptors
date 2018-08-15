ebabe1231a659eebc16db4d0c04de90b91e0a799
Alexis de Talhouët <adetalhouet89@gmail.com>
2018-08-15 14:30:43 -0400

    Added netbox client to get or delete ip
    UT: 80% Classes, 63% lines covered
    
    Change-Id: Ied317c7f251936ced116b6a3ea81789c82095df6
    Issue-ID: CCSDK-462
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

---
+++ netbox-client/provider/src/main/java/org/onap/ccsdk/sli/adaptors/netbox/model/Status.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2018 Bell Canada.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.ccsdk.sli.adaptors.netbox.model;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
+public class Status {
+
+    private Integer value;
+    private String label;
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(final Integer value) {
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(final String label) {
+        this.label = label;
+    }
+
+    public JsonElement toJson() {
+        return new JsonPrimitive(value);
+    }
+
+    public enum Values {
+        ACTIVE(1, "Active"),
+        RESERVED(2, "Reserved");
+
+        private final int value;
+        private final String label;
+
+        Values(final int value, final String label) {
+            this.value = value;
+            this.label = label;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public Status getStatus() {
+            final Status status = new Status();
+            status.setValue(value);
+            status.setLabel(label);
+            return status;
+        }
+
+    }
+}
