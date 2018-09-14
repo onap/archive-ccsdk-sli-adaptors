b46892cf1f0374dc18bec49b6a209b0e6a13ba1a
Stan Bonev <sb5356@att.com>
2018-09-14 15:25:55 -0400

    RA: Expose resource locking as DG node
    Change-Id: Ic12c8c7c59baf29630dab442de64a66dc40bc02e
    Issue-ID: CCSDK-573
    Signed-off-by: Stan Bonev <sb5356@att.com>

---
+++ resource-assignment/provider/src/main/java/org/onap/ccsdk/sli/adaptors/ra/ResourceLockNode.java
@@ -0,0 +1,93 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * openECOMP : SDN-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 			reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.adaptors.ra;
+
+import java.security.SecureRandom;
+import java.util.Map;
+import org.onap.ccsdk.sli.adaptors.lock.comp.LockHelper;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicJavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourceLockNode implements SvcLogicJavaPlugin {
+
+    private static final Logger log = LoggerFactory.getLogger(ResourceLockNode.class);
+
+    private LockHelper lockHelper;
+
+    public void setLockHelper(LockHelper lockHelper) {
+        this.lockHelper = lockHelper;
+    }
+
+    public void lockResource(Map<String, String> paramMap, SvcLogicContext ctx) throws SvcLogicException {
+        String resourceName = getParam(paramMap, "resource-name", true, null);
+        String lockRequester = getParam(paramMap, "lock-requester", false, generateLockRequester());
+        String lockTimeoutStr = getParam(paramMap, "lock-timeout", false, "600"); // Default lock timeout: 10 min
+        int lockTimeout = Integer.parseInt(lockTimeoutStr);
+
+        lockHelper.lock(resourceName, lockRequester, lockTimeout);
+    }
+
+    public void unlockResource(Map<String, String> paramMap, SvcLogicContext ctx) throws SvcLogicException {
+        String resourceName = getParam(paramMap, "resource-name", true, null);
+
+        lockHelper.unlock(resourceName, false);
+    }
+
+    public void lockResource(String resourceName, String lockRequester, int lockTimeout /* sec */) {
+        if (lockRequester == null) {
+            lockRequester = generateLockRequester();
+        }
+        if (lockTimeout <= 0) {
+            lockTimeout = 600;
+        }
+
+        lockHelper.lock(resourceName, lockRequester, lockTimeout);
+    }
+
+    public void unlockResource(String resourceName) {
+        lockHelper.unlock(resourceName, false);
+    }
+
+    private String getParam(Map<String, String> paramMap, String name, boolean required, String def)
+            throws SvcLogicException {
+        String v = paramMap.get(name);
+        if (v != null && v.trim().length() > 0) {
+            log.info("Param: " + name + ": " + v.trim());
+            return v.trim();
+        }
+        if (required) {
+            throw new SvcLogicException("The following node parameter is required: " + name);
+        }
+
+        log.info("Param: " + name + " not supplied. Using default: " + def);
+        return def;
+    }
+
+    private static String generateLockRequester() {
+        SecureRandom rand = new SecureRandom();
+        return "SynchronizedFunction-" + (int) (rand.nextDouble() * 1000000);
+    }
+}
+
---
+++ resource-assignment/provider/src/test/java/jtest/org/onap/ccsdk/sli/adaptors/ra/TestResourceLockNode.java
@@ -0,0 +1,35 @@
+package jtest.org.onap.ccsdk.sli.adaptors.ra;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.onap.ccsdk.sli.adaptors.ra.ResourceLockNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestResourceLockNode {
+
+    @SuppressWarnings("unused")
+    private static final Logger log = LoggerFactory.getLogger(TestResourceLockNode.class);
+
+    @Autowired
+    private ResourceLockNode resourceLockNode;
+
+    @Test
+    public void test1() throws Exception {
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("resource-name", "test-resource-1");
+
+        resourceLockNode.lockResource(paramMap, null);
+        resourceLockNode.unlockResource(paramMap, null);
+    }
+}
