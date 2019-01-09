b9b6b27bcb1ee88581c2da9ba7d7cc1baeb50aa2
Sandeep J <sandeejh@in.ibm.com>
2019-01-09 16:23:38 +0530

    fixed sonar issue in GenericRequest.java
    fixed sonar issue
    
    Issue-ID: CCSDK-525
    Change-Id: Ice555ba2ca3ae91fd8c745d512d7615f214ffe09
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- aai-service/provider/src/main/java/org/onap/ccsdk/sli/adaptors/aai/GenericRequest.java
+++ aai-service/provider/src/main/java/org/onap/ccsdk/sli/adaptors/aai/GenericRequest.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2017 AT&T Intellectual Property. All rights
  *             reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -70,7 +72,7 @@ public class GenericRequest extends AAIRequest {
 
         String request_url = getRequestPath(resourceName);
 
-        Map<String, String> queryParams = new HashMap<String, String> ();
+        Map<String, String> queryParams = new HashMap<> ();
         if(resourceVersion != null) {
             queryParams.put("resource-version", resourceVersion.toString());
         }
