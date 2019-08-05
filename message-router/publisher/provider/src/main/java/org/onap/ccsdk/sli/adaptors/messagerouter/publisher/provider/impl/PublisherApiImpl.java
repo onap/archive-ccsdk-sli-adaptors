5ca03ea380336c59660961d5a69398b688000aa3
Thugutla Sailakshmi <tsaila10@in.ibm.com>
2019-08-05 20:09:46 +0530

    CCSDK-1559-making two final fields as static too
    making two final fields as static too
    
    Issue-ID: CCSDK-1559
    Change-Id: I93fe8b4f16270f17c01f1ad80b43a6be1da9fdc6
    Signed-off-by: Thugutla Sailakshmi <tsaila10@in.ibm.com>

--- message-router/publisher/provider/src/main/java/org/onap/ccsdk/sli/adaptors/messagerouter/publisher/provider/impl/PublisherApiImpl.java
+++ message-router/publisher/provider/src/main/java/org/onap/ccsdk/sli/adaptors/messagerouter/publisher/provider/impl/PublisherApiImpl.java
@@ -1,3 +1,25 @@
+/**
+ * ============LICENSE_START====================================================
+ * org.onap.aaf
+ * ===========================================================================
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * Modifications Copyright (C) 2019 IBM.
+ * ===========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END====================================================
+ *
+ */
 package org.onap.ccsdk.sli.adaptors.messagerouter.publisher.provider.impl;
 
 import java.io.BufferedReader;
@@ -16,8 +38,8 @@ import org.slf4j.LoggerFactory;
 
 public class PublisherApiImpl implements PublisherApi {
 	private static final Logger logger = LoggerFactory.getLogger(PublisherApiImpl.class);
-	protected final Integer DEFAULT_CONNECT_TIMEOUT = 30000; // will be treated as 30 seconds
-	protected final Integer DEFAULT_READ_TIMEOUT = 180000; // will be treated as 3 minutes
+	protected static final Integer DEFAULT_CONNECT_TIMEOUT = 30000; // will be treated as 30 seconds
+	protected static final Integer DEFAULT_READ_TIMEOUT = 180000; // will be treated as 3 minutes
 	private String authorizationString;
 	protected Integer connectTimeout;
 	protected Integer readTimeout;
