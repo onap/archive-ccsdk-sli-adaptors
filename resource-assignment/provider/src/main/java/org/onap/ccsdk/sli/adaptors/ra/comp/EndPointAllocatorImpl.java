c4a0e4279c770957fe2d12296418883897df8087
Thugutla Sailakshmi <tsaila10@in.ibm.com>
2019-08-05 20:36:45 +0530

    CCSDK-1559-return this expression instead of assigning it to a temporary variable
    return this expression instead of assigning it to a temporary variable
    
    Issue-ID: CCSDK-1559
    Change-Id: I1b0ce8b89f51f2fd5b2da7d8f2401d2be5007615
    Signed-off-by: Thugutla Sailakshmi <tsaila10@in.ibm.com>

--- resource-assignment/provider/src/main/java/org/onap/ccsdk/sli/adaptors/ra/comp/EndPointAllocatorImpl.java
+++ resource-assignment/provider/src/main/java/org/onap/ccsdk/sli/adaptors/ra/comp/EndPointAllocatorImpl.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights
  * 						reserved.
+ * 
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -227,8 +229,8 @@ public class EndPointAllocatorImpl implements EndPointAllocator {
 
             log.info("ResourceName:" + r.resourceKey.resourceName + " assetId:" + r.resourceKey.assetId);
 
-            ResourceData rd = getResourceData(r);
-            rdlist.add(rd);
+           
+            rdlist.add(getResourceData(r));
         }
 
         return rdlist;
