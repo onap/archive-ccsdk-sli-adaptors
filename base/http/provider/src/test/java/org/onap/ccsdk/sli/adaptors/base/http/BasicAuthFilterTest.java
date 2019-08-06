0d6c48c0c2b7bc372f45152ca3e5d24126d70387
Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
2019-08-06 19:36:13 +0000

    introduce base adaptor
    introduce base adaptor for other adaptors to extend
    
    Issue-ID: CCSDK-1595
    Signed-off-by: Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
    Change-Id: I35d4499491cd87a7d651fa9ed654ad7c677ae2b8

---
+++ base/http/provider/src/test/java/org/onap/ccsdk/sli/adaptors/base/http/BasicAuthFilterTest.java
@@ -0,0 +1,20 @@
+package org.onap.ccsdk.sli.adaptors.base.http;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class BasicAuthFilterTest {
+    
+    @Test
+    public void notNullParameters() throws Exception {
+        BasicAuthFilter myFilter = new BasicAuthFilter("hello","world");
+        assertNotNull(myFilter);
+    }
+    
+    @Test
+    public void nullParameters() throws Exception {
+        BasicAuthFilter myFilter = new BasicAuthFilter(null,null);
+        assertNotNull(myFilter);
+    }
+}
